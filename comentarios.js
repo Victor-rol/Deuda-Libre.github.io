26337583ffdf0627772b1cc6c14ca2c1632bfad7
Victor-rol <rojasvictor.g2025@gmail.com>
2025-02-24 17:48:52 -0500

    Update and rename testimonios.js to comentarios.js

--- comentarios.js
+++ comentarios.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const form = document.querySelector('.testimonial-form');
+  const form = document.querySelector('.comentario-form');
   form.addEventListener('submit', function(e) {
     // Aquí podrías implementar validaciones adicionales o enviar datos vía AJAX.
     // Por ahora, se muestra un mensaje de agradecimiento.
-    alert('¡Gracias por compartir tu testimonio!');
+    alert('¡Gracias por compartir tu comentario!');
   });
 });
